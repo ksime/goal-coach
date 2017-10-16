10e52f19a1a0eabd4778bf8042bcbf6c1a8b07e5
Max <ksime@protonmail.com>
2017-10-16 16:07:58 +0300

    initial commit

---
+++ src/components/SignUp.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component {
+  render() {
+    return (
+      <div className="form-inline">
+        <h2>Sign Up</h2>
+        <div className="form-group">
+          <input />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default SignUp;
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+  apiKey: 'AIzaSyBaZb3wp3nYzEi8H6dhlpDFS8MJcdha4aw',
+  authDomain: 'goal-coach-648c2.firebaseapp.com',
+  databaseURL: 'https://goal-coach-648c2.firebaseio.com',
+  projectId: 'goal-coach-648c2',
+  storageBucket: '',
+  messagingSenderId: '219640536132',
+};
+
+export const firebaseApp = firebase.initializeApp(config);
